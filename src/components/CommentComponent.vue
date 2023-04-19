663952dc1a34b31e9555fdf15104f4f918d8d773
hoijoii <oodh3328@gmail.com>
2023-04-19 14:40:34 +0900

    Design: Update comment design(svg&font color etc)

--- src/components/CommentComponent.vue
+++ src/components/CommentComponent.vue
@@ -1,34 +1,35 @@
 <template>
   <div id="container" class="comment">
-    <div class="content">
+    <div class="comment-content">
       <div class="score">
         <div class="score-wrapper">
-          <div><img src="@/assets/images/icon-plus.svg"></div>
+          <!-- <div><img src="@/assets/images/icon-plus.svg"></div> -->
+          <inline-svg :src="require('@/assets/images/icon-plus.svg')"/>
           <div class="num">{{ score }}</div>
-          <div><img src="@/assets/images/icon-minus.svg"></div>
+          <inline-svg :src="require('@/assets/images/icon-minus.svg')"/>
         </div>
       </div>
       <div class="content-wrapper">
         <div class="top">
           <img :src="require(`@/assets/images/avatars/${user.image.png}`)" class="profile"/>
-          <div class="mg-lft8">{{ user.username }}</div>
-          <div class="created-at mg-lft8">{{ createdAt }}</div>
+          <div class="mg-lft13">{{ user.username }}</div>
+          <div class="mg-lft13 grayish-blue">{{ createdAt }}</div>
           <button class="reply-btn">
-            <img src="@/assets/images/icon-reply.svg">
-            <span class="mg-lft8">Reply</span>
+            <inline-svg :src="require('@/assets/images/icon-reply.svg')" />
+            <span class="mg-lft7">Reply</span>
           </button>
         </div>
         <div class="body">
-          <div class="text">{{ content }}</div>
+          <div class="text grayish-blue">{{ content }}</div>
         </div>
       </div>
-      
     </div>
   </div>
 </template>
 
 <script setup lang='ts'>
 import { ref, Ref, defineProps } from 'vue'
+import InlineSvg from 'vue-inline-svg'
 
 const props = defineProps({
   id: Number,
