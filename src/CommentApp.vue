54283f1f678c34eac9c60737691ebf34e2d68041
hoijoii <oodh3328@gmail.com>
2023-04-19 11:15:35 +0900

    Feat: Comment component & design

--- src/CommentApp.vue
+++ src/CommentApp.vue
@@ -1,9 +1,31 @@
 <template>
-  <div>
-    work?
+  <div id="container">
+    <comment-component 
+        v-for="(comment, idx) in comments" :key="idx"
+        :id="idx"
+        :content="comment.content"
+        :createdAt="comment.createdAt"
+        :score="comment.score"
+        :user="comment.user"
+        :replies="comment.replies"
+        class="comment-com"
+    />
   </div>
 </template>
 
+<script lang="ts" setup>
+import CommentComponent from '@/components/CommentComponent.vue'
+import { useCommentsStore } from '@/stores/comments'
+import { IComment, IUser } from './types/comments'
+
+const commentsStore = useCommentsStore()
+
+const comments: Array<IComment> = commentsStore.data.comments
+const currentUser: IUser = commentsStore.data.currentUser
+
+</script>
+
 <style lang="scss">
 @import '@/assets/scss/default.scss';
+@import '@/assets/scss/commons.scss';
 </style>
---
+++ src/components/CommentComponent.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="container" class="comment">
+    <div class="content">
+      <div class="score">
+        <div class="score-wrapper">
+          <div><img src="@/assets/images/icon-plus.svg"></div>
+          <div class="num">{{ score }}</div>
+          <div><img src="@/assets/images/icon-minus.svg"></div>
+        </div>
+      </div>
+      <div class="content-wrapper">
+        <div class="top">
+          <img :src="require(`@/assets/images/avatars/${user.image.png}`)" class="profile"/>
+          <div class="mg-lft8">{{ user.username }}</div>
+          <div class="created-at mg-lft8">{{ createdAt }}</div>
+          <button class="reply-btn">
+            <img src="@/assets/images/icon-reply.svg">
+            <span class="mg-lft8">Reply</span>
+          </button>
+        </div>
+        <div class="body">
+          <div class="text">{{ content }}</div>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+</template>
+
+<script setup lang='ts'>
+import { ref, Ref, defineProps } from 'vue'
+
+const props = defineProps({
+  id: Number,
+  content: String,
+  createdAt: String,
+  score: Number,
+  user: {
+    Type: Object,
+    default: {
+      image: { png: '', webp: ''},
+      username: ''
+    }
+  },
+  replies: {
+    Type: Array,
+    default: () => [{
+      id: 0,
+      content: '',
+      createdAt: '',
+      score: 0,
+      replyingTo: '',
+      user: {},
+    }]
+  }
+})
+
+
+</script>
