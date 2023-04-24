cd3665499660e9f6d904ad4bd29b51ded4a2b54c
hoijoii <oodh3328@gmail.com>
2023-04-24 14:21:51 +0900

    Feat: Delete alert

---
+++ src/components/AlertComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="msg">
+    <div class="pop-up">
+      <div class="title">Delete comment</div>
+      <div class="warning grayish-blue">
+        Are you sure you want to delete this comment? 
+        This will remove the comment and can't be undone.
+      </div>
+      <div class="btns">
+        <button class="no">NO, CANCEL</button>
+        <button class="yes" @click="commentsStore.deletePopup = false">YES, DELETE</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang='ts'>
+import { useCommentsStore } from '@/stores/comments';
+
+const commentsStore = useCommentsStore()
+
+const deleteComment = () => {
+  commentsStore.deletePopup = false
+  commentsStore.deleteComment()
+}
+</script>
