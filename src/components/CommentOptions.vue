ea868580faca34a73296a1ae79b324e884f6ca96
hoijoii <oodh3328@gmail.com>
2023-05-10 13:36:51 +0900

    Refactor: icons path

--- src/components/CommentOptions.vue
+++ src/components/CommentOptions.vue
@@ -2,17 +2,17 @@
   <div class="buttons mg-auto">
     <div class="not-me reply" v-if="user.username !== commentsStore.currentUser.username">
       <button class="reply-btn top-btn" @click="emit('replyBtn')">
-        <inline-svg :src="require('@/assets/images/icon-reply.svg')" />
+        <inline-svg :src="require('../assets/images/icon-reply.svg')" />
         <span class="mg-lft7">Reply</span>
       </button>
     </div>
     <div class="my-btn" v-else>
       <button class="delete top-btn" @click="deleteBtn">
-        <inline-svg :src="require('@/assets/images/icon-delete.svg')" />
+        <inline-svg :src="require('../assets/images/icon-delete.svg')" />
         <span class="mg-lft7">Delete</span>
       </button>
       <button class="edit top-btn mg-lft13" @click="emit('editBtn')">
-        <inline-svg :src="require('@/assets/images/icon-edit.svg')" />
+        <inline-svg :src="require('../assets/images/icon-edit.svg')" />
         <span class="mg-lft7">Edit</span>
       </button>
     </div>
